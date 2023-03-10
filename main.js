238320903f0b8df5f104a5673c6bcfdb28691078
Shunsuke Tsuge <stsuge1211@gmail.com>
2023-03-11 00:25:36 +0900

    状態変更ボタンと削除ボタンが動作するように変更

--- main.js
+++ main.js
@@ -22,29 +22,37 @@ const app = new Vue({
         todos: [],
     },
     methods: {
-        // ToDo 追加の処理
-        doAdd:
-            function (event, value) {
-                // ref で名前を付けておいた要素を参照
-                var comment = this.$refs.comment
+        // ToDo追加の処理
+        doAdd: function (event, value) {
+            // ref で名前を付けておいた要素を参照
+            var comment = this.$refs.comment
 
-                // 入力がなければ何もしないで return
-                if (!comment.value.length) {
-                    return
-                }
-
-                // { 新しいID, コメント, 作業状態 }
-                // というオブジェクトを現在の todos リストへ push
-                // 作業状態「state」はデフォルト「作業中=0」で作成
-                this.todos.push({
-                    id: todoStorage.uid++,
-                    comment: comment.value,
-                    state: 0
-                })
-
-                // フォーム要素を空にする
-                comment.value = ''
+            // 入力がなければ何もしないで return
+            if (!comment.value.length) {
+                return
             }
+
+            // { 新しいID, コメント, 作業状態 }
+            // というオブジェクトを現在の todos リストへ push
+            // 作業状態「state」はデフォルト「作業中=0」で作成
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+
+            // フォーム要素を空にする
+            comment.value = ''
+        },
+        // 状態変更の処理
+        doChangeState: function (item) {
+            item.state = item.state ? 0 : 1
+        },
+        // 削除の処理
+        doRemove: function (item) {
+            var index = this.todos.indexOf(item)
+            this.todos.splice(index, 1)
+        }
     },
     watch: {
         // オプションを使う場合はオブジェクト形式にする
